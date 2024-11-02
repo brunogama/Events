e175d1b26cbcbb2f287049ba27925c23b0f9b43e
Bruno da Gama Porciuncula <bgamap@gmail.com>
2024-11-01 21:20:09 -0300

    Initial Commit

---
+++ Events/EventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventsApp.swift
+//  Events
+//
+//  Created by Bruno on 01/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
