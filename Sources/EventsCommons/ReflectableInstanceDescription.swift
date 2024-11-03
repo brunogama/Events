520bf6be374e101960f1706f15af173bc8ef32e0
Bruno da Gama Porciuncula <bgamap@gmail.com>
2024-11-03 19:50:44 -0300

    Revert "mirror na queue"
    This reverts commit d646e43eb54d00ff63a5d64c1dd3391413081f54.

--- Sources/EventsCommons/ReflectableInstanceDescription.swift
+++ Sources/EventsCommons/ReflectableInstanceDescription.swift
@@ -44,21 +44,18 @@ package final class ValueDescriptionBuilder: Sendable {
         DefaultHandler(),
     ]
 
-    package let queue = DispatchQueue(label: "ValueDescriptionBuilder", qos: .utility)
+    package let queue = DispatchQueue(label: "ValueDescriptionBuilder")
 
     private init() {}
 
     package func buildDescription(of value: Any) -> String {
-        queue.sync { [weak self] in
-            guard let self = self else { return "\(value)" }
-            let mirror = Mirror(reflecting: value)
-            for handler in handlers {
-                if handler.canHandle(mirror.displayStyle) {
-                    return handler.handle(value, builder: self)
-                }
+        let mirror = Mirror(reflecting: value)
+        for handler in handlers {
+            if handler.canHandle(mirror.displayStyle) {
+                return handler.handle(value, builder: self)
             }
-            return "\(value)"
         }
+        return "\(value)"
     }
 }
 
