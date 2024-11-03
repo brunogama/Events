a6b5e94eb06729897e12e1bd47370a9c9c728568
Bruno da Gama Porciuncula <bgamap@gmail.com>
2024-11-03 15:12:28 -0300

    fix package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Events",
-    platforms: [.iOS(.v17)],
+    platforms: [.iOS(.v17), .macOS(.v14)],
     products: [
         .library(
             name: "EventsCommons",
