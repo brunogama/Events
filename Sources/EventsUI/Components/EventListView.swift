6a1ff93dd28c47cd7c9ead6d73b02fe2413abc85
Bruno da Gama Porciuncula <bgamap@gmail.com>
2024-11-03 18:34:47 -0300

    limpando a lista onAppear

--- Sources/EventsUI/Components/EventListView.swift
+++ Sources/EventsUI/Components/EventListView.swift
@@ -38,6 +38,8 @@ public struct EventListView: View {
                 }
             }
             .background(Color.dynamicBackground.ignoresSafeArea())
+        }.onAppear {
+            events = []
         }
     }
 }
