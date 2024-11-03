3682b076dfa481b871d906dfea8c8d1fc7781db3
Bruno da Gama Porciuncula <bgamap@gmail.com>
2024-11-03 15:06:39 -0300

    added app

---
+++ Examples/App/Sources/EventsCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventsCombineApp.swift
+//  EventsCombine
+//
+//  Created by Bruno on 03/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EventsCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
