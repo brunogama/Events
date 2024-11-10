65a586c6de1a105d9d987481e6075594f3291bf7
Bruno da Gama Porciuncula <bgamap@gmail.com>
2024-11-10 17:23:06 -0300

    pre subscription token

---
+++ Sources/EventsDomain/Models/SubscriptionToken.swift
@@ -0,0 +1,26 @@
+//
+//  SubscriptionToken.swift
+//  EnventConsumerSimulation
+//
+//  Created by Bruno on 10/11/24.
+//
+
+import Combine
+
+public class SubscriptionToken {
+    private var cancellable: AnyCancellable?
+    
+    deinit {
+        cancel()
+    }
+    
+    public func store(_ cancellable: AnyCancellable) {
+        self.cancellable?.cancel()
+        self.cancellable = cancellable
+    }
+    
+    public func cancel() {
+        cancellable?.cancel()
+        cancellable = nil
+    }
+}
